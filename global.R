7aaeae65bef075322c06b1d46084d2197f246d83
Joe <joedean83@gmail.com>
2018-12-21 09:50:05 -0500

    added helper files based on tutorial

---
+++ global.R
@@ -0,0 +1,24 @@
+
+#all that has to be done at beginning
+
+library(shiny)
+library(leaflet)
+library(ggmap)
+library(here)
+library(maptools)
+library(readr)
+library(dplyr)
+
+
+df <- read_csv(here::here('census_data.csv'))
+
+language_list <- unique(df$Language)
+
+zipcodes.shp <- readShapeSpatial(here::here('shape-files',
+                                            'cb_2016_us_zcta510_500k',
+                                            'cb_2016_us_zcta510_500k.shp'))
+
+mapped <- subset(zipcodes.shp, ZCTA5CE10 %in% df$Zip)
+
+
+start_loc <- geocode('Atlanta, GA', source="dsk")
