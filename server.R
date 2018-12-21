ffc1cf8dcdd0cb3aad6d2fdf19f0eb5242087dc1
Joe <joedean83@gmail.com>
2018-12-21 09:49:39 -0500

    cleaned up file

--- server.R
+++ server.R
@@ -1,37 +1,10 @@
 
-# This is the server logic for a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
-library(shiny)
-library(leaflet)
-library(ggmap)
-library(here)
-library(maptools)
-library(readr)
-library(dplyr)
-
-df <- read_csv(here::here('census_data.csv'))
-
-language_list <- unique(df$Language)
-
-zipcodes.shp <- readShapeSpatial(here::here('shape-files',
-                                            'cb_2016_us_zcta510_500k',
-                                            'cb_2016_us_zcta510_500k.shp'))
-
-mapped <- subset(zipcodes.shp, ZCTA5CE10 %in% df$Zip)
-
-
-start_loc <- geocode('Atlanta, GA', source="dsk")
-
-
 
 shinyServer(function(input, output) {
   
   mylang <- reactive({
-    df %>% filter(Language==input$language) %>% #Choose language here!
+    df %>% 
+      filter(Language==input$language) %>% #Choose language here!
       mutate(Zip = sprintf('%05d',Zip)) %>%
       filter(`Estimate Total` > 0) %>%
       arrange(Zip)
